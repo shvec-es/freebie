8ba405ae6b18c7f11531b5145fb6717d787f36c3
shvec-es <82171958+shvec-es@users.noreply.github.com>
2022-11-22 16:19:19 +0000

    Deploying to gh-pages from @ shvec-es/freebie@0a9ccc62db3ca760259274113f7eec5b86a0b4a1 🚀

---
+++ index.991b1ded.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelectorAll("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}[...e.openModalBtn].map((e=>e.addEventListener("click",o))),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.991b1ded.js.map
