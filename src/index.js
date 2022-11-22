0f061b0156750d9b10a8cfb0e0e86f85da2a887c
Evgeniya Shvec <82171958+shvec-es@users.noreply.github.com>
2022-11-22 18:17:53 +0200

    add modal, slick, animation

---
+++ src/index.js
@@ -0,0 +1,13 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+  [...refs.openModalBtn].map(el => el.addEventListener('click', toggleModal));
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
